0ad47a095911ec1585123725cbc075bc933449c1
萧韵水 <10528653+xiao-yunshui@user.noreply.gitee.com>
2022-09-25 19:44:38 +0800

    init gshop

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入路由器
+import router from '@/router'
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+    router,
+
+})
